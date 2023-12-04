55dd90e4a25ffbc265d79c51986ac87cf288d1f4
Zikrul Khalis <khaliszikrul16@gmail.com>
2023-12-05 01:13:37 +0700

    pertemuan 28 November

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous" />
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master')
 
-	<h2>www.malasngoding.com</h2>
+@section('title', 'Data Pegawai')
+
+@section('konten')
+
+	<h2>malasngoding corp</h2>
 	<h3>Data Pegawai</h3>
 
 	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
 
 	<br/>
-	<br/>
+    <p>Cari Data Pegawai : berdasarkan nama</p>
+	<form action="/pegawai/cari" method="GET">
 
-	<table class="table table-striped">
+		<input class="form-control" type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old("cari", isset($cari) ? $cari : '') }}">
+		<input class="mt-1 mb-3 btn btn-primary" type="submit" value="CARI">
+	</form>
+
+	<table class="table table-striped table-hover">
 		<tr>
 			<th>Nama</th>
 			<th>Jabatan</th>
@@ -23,22 +26,18 @@
 			<th>Opsi</th>
 		</tr>
 		@foreach($pegawai as $p)
-<tr >
-	<td >{{ $p->pegawai_nama }}</td>
-	<td >{{ $p->pegawai_jabatan }}</td>
-	<td >{{ $p->pegawai_umur }}</td>
-	<td >{{ $p->pegawai_alamat }}</td>
-	<td >
-            <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
-            <a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-primary">View</a>
-            <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
-	</td>
-</tr>
-@endforeach
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
 	</table>
-<br/>
-	{{ $pegawai->links() }}
-
-
-</body>
-</html>
+    {{$pegawai->links()}}
+@endsection
